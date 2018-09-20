564e1c0c770d72333cec65f34a836b13f3e87627
I. Mina Ruum <Y.Motara@ru.ac.za>
2018-09-20 11:39:46 +0200

    Add very basic class & accessibility modifiers example.

---
+++ WithClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace WithClass
+{
+    public class Animal
+    {
+        string species;
+        public Animal(string species, string sound)
+        {
+            this.species = species;
+            Sound = sound;
+        }
+        public string Sound { get; private set; }
+        public string Speak()
+        {
+            return Sound + " " + Sound + "!";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cow = new Animal("taurus", "moo");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(cow.Sound);
+            //cow.Sound = "woof";
+            Console.WriteLine(cow.Speak());
+            Console.ReadLine();
+        }
+    }
+}
